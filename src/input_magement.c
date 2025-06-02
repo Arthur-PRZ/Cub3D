7485e0f520fa86b6dc37f70d50cf36830e5d5383
Arthur Perez <artperez@c3r3p2.42perpignan.fr>
2025-06-02 10:14:47 +0200

    input management done

---
+++ src/input_magement.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_magement.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artperez <artperez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 10:01:13 by artperez          #+#    #+#             */
+/*   Updated: 2025/06/02 10:12:39 by artperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3D.h"
+
+void check_input(int args_nbr, char *input)
+{
+    if (args_nbr <= 1)
+    {
+        write(2, "file .cub is needed\n", 21);
+        exit;
+    }
+    if (args_nbr >= 3)
+    {
+        write(2, "Only one file .cub is needed\n", 30);
+        exit;
+    }
+    check_map_name(input);
+}
+
+int	check_map_name(char *map_name)
+{
+	int	i;
+	int	len;
+
+	i = ft_strlen(map_name);
+	len = ft_strlen(map_name);
+	while (i > 0)
+	{
+		if (map_name[i] == '.')
+		{
+			if (ft_strncmp(ft_strnstr(map_name + i, ".cub", 4), ".cub",
+					len) == 0)
+				return (0);
+		}
+		i--;
+	}
+    write(2, "file .cub is needed\n", 21);
+    exit;
+}
